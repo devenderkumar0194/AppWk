4cb2b8eaecc376c017bb55e420d82b498c578f9f
Development <development.emi@gmail.com>
2025-05-17 07:40:53 +0530

    update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const Stripe = require('stripe');
 require('dotenv').config();
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY); 
 
 const app = express();
 const cookieParser = require('cookie-parser');
