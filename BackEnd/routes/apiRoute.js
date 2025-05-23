52312be5bded7b1d3d20108e3e71b95adb920709
Development <development.emi@gmail.com>
2025-05-24 01:46:37 +0530

    update

--- BackEnd/routes/apiRoute.js
+++ BackEnd/routes/apiRoute.js
@@ -2,6 +2,11 @@ const express = require('express');
 const apiRouter = express.Router();
 
 const TransactionController = require('../controller/TransactionController');
+const UserController = require('../controller/UserController');
+
+
+apiRouter.post("/register", UserController.register);
+apiRouter.post("/login", UserController.login);
 
 apiRouter.post("/add-trns", TransactionController.addTrns);
 apiRouter.get("/trns-list", TransactionController.trnsList);
